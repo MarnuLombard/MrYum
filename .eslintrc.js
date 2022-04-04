1b9f77badc632e7b0b9e6f131e563d137aaa083d
Marnu Lombard <marnulombard@gmail.com>
2022-04-04 04:59:59 +0200

    [create] Add the path config into our tools to map `@/` to our basedir - Simplifies accessing relative paths - Becomes important as the app grows in sise - Needs some extra dancing to get it to play nice with `jest` and `eslint`

--- .eslintrc.js
+++ .eslintrc.js
@@ -19,17 +19,34 @@ module.exports = {
   plugins: [
     'react',
     '@typescript-eslint',
+    'import',
   ],
+  settings: {
+    'import/parsers': {
+      '@typescript-eslint/parser': ['.ts'],
+    },
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.ts', '.json'],
+        moduleDirectory: ['node_modules', 'src/'],
+      },
+      typescript: {
+        alwaysTryTypes: true,
+      },
+    },
+  },
   rules: {
-    "@typescript-eslint/indent": ["error", 2],
-    "@typescript-eslint/no-unused-vars": "error",
-    "@typescript-eslint/no-explicit-any": "off",
-    "import/prefer-default-export": "off",
-    "import/no-default-export": "error",
-    "import/extensions": "off",
-    "arrow-body-style": "off",
-    "no-useless-constructor": "off",
-    "class-methods-use-this": "off",
-    "no-plusplus": "off"
+    '@typescript-eslint/indent': ['error', 2],
+    '@typescript-eslint/no-unused-vars': 'error',
+    '@typescript-eslint/no-explicit-any': 'off',
+    'import/prefer-default-export': 'off',
+    'import/no-default-export': 'error',
+    'import/extensions': 'off',
+    'arrow-body-style': 'off',
+    'no-useless-constructor': 'off',
+    'class-methods-use-this': 'off',
+    'no-plusplus': 'off',
+    'no-shadow': 'off', // See: https://github.com/typescript-eslint/typescript-eslint/issues/2552#issuecomment-691694839
+    '@typescript-eslint/no-shadow': 'error',
   },
 };
